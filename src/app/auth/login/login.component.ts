cc7db9f539b2014f43c8a0a99adaf1bd44c18f09
Waheed <wahid.khan74@gmail.com>
2022-04-25 17:54:28 +0530

    added  template druiven form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public login:Login = {
+    email: '',
+    password: '',
+    agree: false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onSubmit(form:any) {
+   //  console.log(form);
+    console.log(this.login);
+    
+  }
+}
+
+interface Login  {
+  email :string;
+  password :string;
+  agree:boolean
 }
